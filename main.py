0c2ad38e11e3e797f6320682503f28f88f6ad214
Guilherme Eduardo <cuzforeverisenough@gmail.com>
2025-03-29 17:42:28 -0300

    Initial commit

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ window = pygame.display.set_mode(size=(600,480))
 
 while True:
     # check for all events
-    for event in pygame.event.get():dasdas
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit() # close window
             quit() # end pygame
