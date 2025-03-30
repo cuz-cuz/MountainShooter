7d73afc2c76c07cfe581342967b1b18188f0464b
Guilherme Eduardo <cuzforeverisenough@gmail.com>
2025-03-30 09:59:52 -0300

    Creating player shot Creating enemy shot

--- code/Enemy.py
+++ code/Enemy.py
@@ -1,11 +1,19 @@
 import pygame
-from code.Const import ENTITY_SPEED, PLAYER_KEY_DOWN, PLAYER_KEY_LEFT, PLAYER_KEY_RIGHT, PLAYER_KEY_UP, WIN_HEIGHT, WIN_WIDTH
+from code.Const import ENTITY_SHOT_DELAY, ENTITY_SPEED, PLAYER_KEY_DOWN, PLAYER_KEY_LEFT, PLAYER_KEY_RIGHT, PLAYER_KEY_UP, WIN_HEIGHT, WIN_WIDTH
+from code.EnemyShot import EnemyShot
 from code.Entity import Entity
 
 class Enemy(Entity):
     def __init__(self, name, position):
         super().__init__(name, position)
+        self.shot_delay = ENTITY_SHOT_DELAY[self.name]
 
     def move(self):
         self.rect.centerx -= ENTITY_SPEED[self.name]
+
+    def shoot(self):
+        self.shot_delay -= 1
+        if self.shot_delay == 0:
+            self.shot_delay = ENTITY_SHOT_DELAY[self.name]
+            return EnemyShot(name=f'{self.name}Shot', position=(self.rect.centerx, self.rect.centery))
         
